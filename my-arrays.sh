e974371f21647dddf0ee8474e0fe6376e39d1aa9
tuccie <leratotuccie@gmail.com>
2024-04-24 11:31:47 +0200

    add: for-loop to print all elements of the array

--- my-arrays.sh
+++ my-arrays.sh
@@ -9,3 +9,13 @@ echo "${array[0]}"
 ## Print all elements of the array
 echo "${array[@]}"
 
+## Print size / number of elements in the array
+echo "An array has: ${#array[@]} elements"
+
+## Print Two / 2 elements starting from Fourth:
+echo "${array[@]:3:2}"
+
+## For-Loop to print all the elements of the array
+for item in "${array[@]}"; do
+    echo "$item"
+done
