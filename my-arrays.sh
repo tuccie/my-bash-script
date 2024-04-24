5e7986bd5bf71fccf6b9d9296265cc682b4fcda3
tuccie <leratotuccie@gmail.com>
2024-04-24 11:23:54 +0200

    Print all elements of the array

---
+++ my-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Declare an array with 6 elements:
+array=(one two three four five six)
+
+## Print the first element of the array
+echo "${array[0]}"
+
+## Print all elements of the array
+echo "${array[@]}"
+
